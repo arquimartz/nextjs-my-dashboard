558f118edb65d41399d743ebef441f9a96109db7
Arquimides Martinez <2u1m333@gmail.com>
2025-02-18 22:23:45 -0600

    feat: add SidebarMenuItem component and update Sidebar with navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-	redirect('/dashboard/counter');
-
-	return (
-		<>
-			<h1>Hola mundo</h1>
-		</>
-	)
+	redirect('/dashboard/main');
 }
