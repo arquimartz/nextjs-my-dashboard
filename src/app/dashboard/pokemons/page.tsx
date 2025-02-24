67ded89abbd986d8aef51a5df5d594ef7102512b
Arquimides Martinez <2u1m333@gmail.com>
2025-02-23 21:39:11 -0600

    feat: add PokemonsPage component and define interfaces for API response and simple Pokémon

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -14,13 +14,12 @@ const getPokemons = async (limit: number = 20, offset: number = 0): Promise<Simp
     return pokemons;
 }
 
-
-
 export default async function PokemonsPage() {
     const pokemons = await getPokemons(151);
 
     return (
         <div className="flex flex-col">
+            <span className="text-5xl my-2">Listado de Pokémons <small>estáticos</small></span>
             <div className="flex flex-wrap gap-10 items-center justify-center">
                 {
                     pokemons.map(pokemon => (
